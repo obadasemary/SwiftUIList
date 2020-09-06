6f9b954ce3c2e9bb2ed251a9264262f97e4d9243
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-06 23:44:02 +0300

    A list view with custom row layout
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUIList/ContentView.swift
+++ SwiftUIList/ContentView.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
     var body: some View {
-//        List {
-//
-//            ForEach(1...10, id: \.self) { index in
-//                Text("Item \(index)")
-//            }
-//        }
-        List(1...10, id: \.self) {
-            Text("Item \($0)")
+        
+        List(restaurantNames.indices, id: \.self) { index in
+            
+            HStack {
+                
+                Image(self.restaurantImages[index])
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .cornerRadius(5)
+                
+                Text(self.restaurantNames[index])
+            }
         }
     }
 }
