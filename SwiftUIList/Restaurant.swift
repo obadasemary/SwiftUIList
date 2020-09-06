845119fe538d231c9ab3d2dd3fc00e5dcda56282
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-06 23:50:06 +0300

    Working with a Collection of Data
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ SwiftUIList/Restaurant.swift
@@ -0,0 +1,14 @@
+//
+//  Restaurant.swift
+//  SwiftUIList
+//
+//  Created by Abdelrahman Mohamed on 6.09.2020.
+//
+
+import Foundation
+
+struct Restaurant {
+    
+    var name: String
+    var image: String
+}
