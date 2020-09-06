502a0e301927b82bf823b756b977051858be810e
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-06 05:50:52 +0300

    Update ContentView.swift

--- SwiftUIList/ContentView.swift
+++ SwiftUIList/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            
+            ForEach(1...10, id: \.self) { index in
+                Text("Item \(index)")
+            }
+        }
     }
 }
 
